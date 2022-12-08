522e7d33050dc4e563e958f71b4ecdc3867b3275
Zach S <67885686+ZachBiwi@users.noreply.github.com>
2022-12-08 16:18:47 -0600

    Version 0.1!

---
+++ Main.cpp
@@ -0,0 +1,16 @@
+//Zach Sawicki
+//Very basic program meant to help me learn GIT,
+//a free and open-source standard used by just
+//about all software engineers for version control
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    cout << "Version 0.1";
+    cout << "This program is made to show that I have learned git";
+
+    return 0;
+}
