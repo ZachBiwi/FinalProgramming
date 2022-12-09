5aa6401ac885c6e83b6cc279ad9931e33a19054f
Zach S <67885686+ZachBiwi@users.noreply.github.com>
2022-12-08 18:41:04 -0600

    1.0! master branch

--- Main.cpp
+++ Main.cpp
@@ -4,14 +4,102 @@
 //about all software engineers for version control
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 
 int main() {
-    cout << "Version 0.1";
-    cout << "This program is made to show that I have learned git";
-
+    Printer p;
+    p.setMessage1("Version 0.4");
+    p.setMessage2("This program is made to show that I have learned git");
+    p.printAll();
+    p.setMessage1("I have learned git commands like: ");
+    p.setMessage2("git init");
+    p.printAll();
+    p.setMessage1("git add [name of file]");
+    p.setMessage2("git reset [name of file]");
+    p.printAll();
+    p.setMessage1("git commit (with optional flag -m "" to add a message");
+    p.setMessage2("git branch");
+    p.printAll();
+    p.setMessage1("git checkout (with optional flag -b to create a branch");
+    p.setMessage2("git stash");
+    p.printAll();
+    p.setMessage1("This is my final commit to the beta branch");
+    p.setMessage2("I will then merge this with main");
+    p.printAll();
+    
     return 0;
 }
 
+
+
+/*
+* @@brief class to print one of two messages
+*/
+class Printer {
+private:
+    string message1;
+    string message2;
+public:
+    Printer();
+    Printer(string x, string y);
+    void setMessage1(string s);
+    void setMessage2(string s);
+    void print1(); 
+    void print2();
+    void printAll();
+};
+
+/*
+* @@brief Default constructor that sets message1 and message2 to null
+*/
+Printer::Printer() {
+    message1 = "";
+    message2 = "";
+}
+
+/*
+* @@brief Parameter constructor to set message1 and message2 to arguments
+*/
+Printer::Printer(string x, string y) {
+    message1 = x;
+    message2 = y;
+}
+
+/*
+* @@brief Setter for message1
+*/
+void Printer::setMessage1(string s) {
+    message1 = s;
+}
+
+/*
+* @@brief Setter for message2
+*/
+void Printer::setMessage2(string s) {
+    message2 = s;
+}
+
+/*
+* @@brief Print message1 to console
+*/
+void Printer::print1() {
+    cout << message1 << endl;
+}
+
+/*
+* @@brief Print message2 to console
+*/
+void Printer::print2() {
+    cout << message2 << endl;
+}
+
+/*
+* @@brief Print message1 and message2 to console
+*/
+void Printer::printAll() {
+    print1();
+    print2();
+}
